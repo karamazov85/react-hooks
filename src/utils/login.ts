474a368a160abf943aad9ad53e1d107f6c28b818
karamazov85 <farkas.adam.mail@gmail.com>
2022-10-02 18:34:26 +0100

    typed login util function

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,5 +1,5 @@
 // mocking API response
-export async function logIn (username, password) {
+export async function logIn (username: string, password: string): Promise<void> { // Note: the generic type of the Promise is always the same as the type of the reslove value. Here, we resolve to void as this is just a mocking function.
     return new Promise ((resolve, reject) => {
         setTimeout(() => {
             if (username === 'Stan' && password === 'Getz') {
